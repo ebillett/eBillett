09ab0fe57137d9a117cb9e5484df5ddb04e0adda
Martin Berg <martin@tiltlab.com>
2012-01-19 01:15:16 +0100

    Added login form w/animate to register form. Animate back needs to be done

--- Resources/ui/login.js
+++ Resources/ui/login.js
@@ -1,19 +1,54 @@
 var general = require('ui/styles/general'),
 	styles = require('ui/styles/login'),
+	self = Titanium.UI.createWindow(styles.loginWin),
 	wrapper = Titanium.UI.createView(general.wrapper),
+	logo = Titanium.UI.createView(styles.logo),
 	loginWrapper = Titanium.UI.createView(styles.loginWrapper),
 	usernameField = Titanium.UI.createTextField(styles.usernameField),
 	passwordField = Titanium.UI.createTextField(styles.passwordField),
 	loginBtn = Titanium.UI.createButton(styles.loginBtn),
-	self = Titanium.UI.createWindow(styles.loginWin);
+	loginBtnLabel = Titanium.UI.createLabel(styles.loginBtnLabel),
+	seperator = Titanium.UI.createView(styles.seperator),
+	notRegisteredWrapper = Titanium.UI.createView(styles.notRegisteredWrapper),
+	nrTitle = Titanium.UI.createLabel(styles.nrTitle),
+	nrInfo = Titanium.UI.createLabel(styles.nrInfo),
+	nrLink = Titanium.UI.createLabel(styles.nrLink),
+	continueWrapper = Titanium.UI.createView(styles.continueWrapper),
+	continueLabel = Titanium.UI.createLabel(styles.continueLabel),
+	noLoginAlert = Titanium.UI.createAlertDialog(styles.noLoginAlert),
+	registerWrapper = Titanium.UI.createView(styles.registerWrapper),
+	nameField = Titanium.UI.createTextField(styles.nameField),
+	telField = Titanium.UI.createTextField(styles.telField),
+	repeatPwField = Titanium.UI.createTextField(styles.repeatPwField),
+	backBtn = Titanium.UI.createButton(styles.backBtn);
+	
+	
 
 var layout = function() {
-	
+	wrapper.add(logo);
+
 	// Setup login form
 	loginWrapper.add(usernameField);
 	loginWrapper.add(passwordField);
+	loginBtn.add(loginBtnLabel);
 	loginWrapper.add(loginBtn);
 	wrapper.add(loginWrapper);
+
+	// Setup not registered message
+	notRegisteredWrapper.add(seperator);
+	notRegisteredWrapper.add(nrTitle);
+	notRegisteredWrapper.add(nrInfo);
+	notRegisteredWrapper.add(nrLink);
+	wrapper.add(notRegisteredWrapper);
+
+	// Setup continue without login
+	continueWrapper.add(continueLabel);
+	self.add(continueWrapper);
+
+	// Setup register form
+	registerWrapper.add(nameField);
+	registerWrapper.add(telField);
+	
 
 	self.add(wrapper);
 };
@@ -22,4 +57,107 @@ exports.load = function() {
 	layout();
 
 	self.open({modal: true});
+};
+
+
+// Toggle login or register state for button
+var toggleLoginBtn = function(a) {
+	if(a === 1) {
+		debug('Login button is now for registration');
+
+		loginBtnLabel.text = 'REGISTRER';
+		loginBtn.type = 'register';
+	} else {
+		debug('Login button is now for logging in');
+
+		loginBtnLabel.text = 'LOGG INN';
+		loginBtn.type = 'login';
+	}
+};
+
+loginBtn.addEventListener('click', function() {
+	alert('hei');
+});
+
+// Throw warning about no purchase-mode
+continueWrapper.addEventListener('click', function() {
+	noLoginAlert.show();
+});
+
+noLoginAlert.addEventListener('click', function(e) {
+	if(e.index === 0) {
+		self.close(); // Close login win and go into no-buy mode
+
+		debug('App is in no buy-mode');
+	} else {
+		noLoginAlert.hide();
+	}
+});
+
+notRegisteredWrapper.addEventListener('click', function() {
+	debug('Start registration');
+	animateToRegister();
+});
+
+//
+// Animations
+//
+
+var animateToRegister = function() {
+	// Fade out not registered-text
+	notRegisteredWrapper.animate({
+		top: 262,
+		opacity: 0,
+		duration: 500
+	});
+
+	// Move loginwrapper down
+	loginWrapper.animate({
+		top: 185,
+		duration: 500,
+		delay: 200
+	});
+
+	// Move button further down to make place for verify pw field
+	loginBtn.animate({
+		top: 145,
+		duration: 500,
+		delay: 200
+	});
+
+	// Add extra fields
+	self.add(registerWrapper);
+	registerWrapper.animate({
+		opacity: 100,
+		duration: 500,
+		delay: 500
+	});
+
+	// Add verify pw field
+	self.add(repeatPwField);
+	repeatPwField.animate({
+		opacity: 100,
+		duration: 500,
+		delay: 500
+	});
+
+	// Add back button
+	self.add(backBtn);
+	backBtn.animate({
+		opacity: 100,
+		duration: 500,
+		delay: 700
+	});
+
+	backBtn.addEventListener('click', function() {
+		animateToLogin();
+	});
+
+	// Change to register mode
+	//setTimeout(toggleLoginBtn(1), 2000);
+	toggleLoginBtn(1);
+};
+
+var animateToLogin = function() {
+	
 };
--- Resources/ui/styles/login.js
+++ Resources/ui/styles/login.js
@@ -3,18 +3,24 @@ exports = {
 		backgroundImage: 'images/login/bg.png',
 		navBarHidden: true
 	},
+	logo: {
+		width: 101,
+		height: 28,
+		top: 40,
+		backgroundImage: 'images/common/logo.png'
+	},
 	loginWrapper: {
 		layout: 'vertical',
-		top: 40,
-		height: 360
+		top: 25,
+		height: 150
 	},
 	usernameField: {
-		hintText: 'Brukernavn',
+		hintText: 'Epostadresse',
 		color: '#000',
-		height:50,
+		height:40,
 		width: 245,
 		backgroundColor: '#fff',
-		borderRadius: 6,
+		borderRadius: 4,
 		borderColor: '#bbb',
 		clearButtonMode: Ti.UI.INPUT_BUTTONMODE_ONFOCUS,
 		autocapitalization: Ti.UI.TEXT_AUTOCAPITALIZATION_NONE,
@@ -26,11 +32,11 @@ exports = {
 	passwordField: {
 		hintText: 'Passord',
 		color: '#ccc',
-		height:50,
+		height:40,
 		width: 245,
-		top: 10,
+		top: 6,
 		backgroundColor: '#fff',
-		borderRadius: 6,
+		borderRadius: 4,
 		borderColor: '#bbb',
 		clearButtonMode: Ti.UI.INPUT_BUTTONMODE_ONFOCUS,
 		autocapitalization: Ti.UI.TEXT_AUTOCAPITALIZATION_NONE,
@@ -40,7 +46,156 @@ exports = {
 	},
 	loginBtn: {
 		width: 245,
-		height: 40,
-		title: 'Logg inn'
+		height: 50,
+		top: 10,
+		backgroundImage: 'images/common/greenbtn.png',
+		type: 'login' // Changes to 'register' when user registers profile
+	},
+	loginBtnLabel: {
+		height: 'auto',
+		width: 'auto',
+		text: 'LOGG INN',
+		color: '#eee',
+		touchEnabled: false,
+		font: {
+			fontWeight: 'bold',
+			fontSize: 16
+		},
+		shadowColor: '#516a12',
+		shadowOffset: {x: 0, y:-1}
+	},
+	notRegisteredWrapper: {
+		layout: 'vertical',
+		height: 130,
+		top: 20
+	},
+	nrTitle: {
+		height: 'auto',
+		left: 40,
+		top: 15,
+		touchEnabled: false,
+		text: 'Opprette konto?',
+		color: '#666',
+		font: {
+			fontSize: 18
+		},
+		shadowColor: '#fff',
+		shadowOffset: {x: 0, y:1}
+	},
+	nrInfo: {
+		height: 'auto',
+		left: 40,
+		top: 6,
+		touchEnabled: false,
+		text: 'Får å kjøpe billetter, og bruke telefonen \nsom billett må du ha en konto',
+		color: '#666',
+		font: {
+			fontSize: 14
+		},
+		shadowColor: '#fff',
+		shadowOffset: {x: 0, y:1}
+	},
+	nrLink: {
+		height: 'auto',
+		left: 40,
+		top: 12,
+		touchEnabled: false,
+		text: 'Klikk her får å komme i gang',
+		color: '#516a12',
+		font: {
+			fontSize: 14,
+			fontWeight: 'bold'
+		},
+		shadowColor: '#fff',
+		shadowOffset: {x: 0, y:1}
+	},
+	seperator: {
+		height: 3,
+		backgroundImage: 'images/common/seperator.png'
+	},
+	continueWrapper: {
+		bottom: 0,
+		height: 60,
+		width: 320,
+		zIndex: 500
+	},
+	continueLabel: {
+		height: 'auto',
+		left: 40,
+		top: 18,
+		touchEnabled: false,
+		text: 'Fortsett uten å logge inn  ›',
+		color: '#555',
+		font: {
+			fontSize: 14,
+			fontWeight: 'bold'
+		},
+		shadowColor: '#eee',
+		shadowOffset: {x: 0, y:1}
+	},
+	noLoginAlert: {
+		title: 'Fortsett uten innlogging',
+		message: 'Du kan ikke kjøpe billetter hvis du ikke logger inn.',
+		buttonNames: ['OK', 'Avbryt']
+	},
+	registerWrapper: {
+		layout: 'vertical',
+		height: 90,
+		opacity: 0,
+		top: 92
+	},
+	nameField: {
+		hintText: 'Ditt navn',
+		color: '#000',
+		height:40,
+		width: 245,
+		backgroundColor: '#fff',
+		borderRadius: 4,
+		borderColor: '#bbb',
+		clearButtonMode: Ti.UI.INPUT_BUTTONMODE_ONFOCUS,
+		autocapitalization: Ti.UI.TEXT_AUTOCAPITALIZATION_NONE,
+		left:4,
+		right: 4,
+		autocorrect: false,
+		paddingLeft: 8
+	},
+	telField: {
+		hintText: 'Mobilnummer',
+		color: '#ccc',
+		height:40,
+		width: 245,
+		top: 6,
+		backgroundColor: '#fff',
+		borderRadius: 4,
+		borderColor: '#bbb',
+		clearButtonMode: Ti.UI.INPUT_BUTTONMODE_ONFOCUS,
+		autocapitalization: Ti.UI.TEXT_AUTOCAPITALIZATION_NONE,
+		passwordMask: false,
+		autocorrect: false,
+		paddingLeft: 8
+	},
+	repeatPwField: {
+		hintText: 'Gjenta passord',
+		color: '#ccc',
+		height:40,
+		width: 245,
+		top: 276,
+		opacity: 0,
+		backgroundColor: '#fff',
+		borderRadius: 4,
+		borderColor: '#bbb',
+		clearButtonMode: Ti.UI.INPUT_BUTTONMODE_ONFOCUS,
+		autocapitalization: Ti.UI.TEXT_AUTOCAPITALIZATION_NONE,
+		passwordMask: true,
+		autocorrect: false,
+		paddingLeft: 8
+	},
+	backBtn: {
+		width: 24,
+		height: 20,
+		backgroundImage: 'images/login/back.png',
+		top: 60,
+		left: 40,
+		opacity: 0
 	}
 };
