f8f1ce806cd158c27535550b57d90fc312390e5c
Martin Berg <martin@tiltlab.com>
2012-01-19 23:11:50 +0100

    Disable layer, when app state = limited

---
+++ Resources/ui/styles/tabgroup.js
@@ -0,0 +1,18 @@
+exports = {
+	tabgroup: {
+		backgroundImage: 'images/common/bg.png'
+	},
+	disable: {
+		backgroundColor: '#000',
+		opacity: 0.3,
+		height:50,
+		width:200,
+		bottom:0,
+		right: 0
+	},
+	loginAlert: {
+		title: 'Ikke innlogget',
+		message: 'Du må logge inn for å få tilgang til dette. \n Vil du logge inn nå?',
+		buttonNames: ['Ja', 'Nei']
+	}
+}
