066f80c662e84d9300f2bc7cfd4b3611cf8a3e50
Martin Berg <martin@tiltlab.com>
2012-03-09 11:25:07 +0100

    Setup profile styling

--- Resources/ui/profile/Profile.js
+++ Resources/ui/profile/Profile.js
@@ -4,18 +4,40 @@ var general = require('ui/styles/general'),
 	u = require('plugins/utils'),
 	self = Titanium.UI.createWindow(general.defaultWindow),
 	wrapper = Titanium.UI.createView(general.wrapper),
+	userHeader = Titanium.UI.createView(styles.tableHeader),
+	userHeaderTitle = Titanium.UI.createLabel(styles.tableHeaderTitle),
 	table = Titanium.UI.createTableView(styles.table),
+	easyHeader = Titanium.UI.createView(styles.tableHeader),
+	easyHeaderTitle = Titanium.UI.createLabel(styles.tableHeaderTitle),
+	easyWrapper = Titanium.UI.createView(styles.easyWrapper),
+	easyLabel = Titanium.UI.createLabel(styles.easyLabel),
 	logoutBtn = Titanium.UI.createButton(styles.logoutBtn);
 
 
 	self.titleControl = general.defaultTitle('Min profil');
+	self.add(general.shadowTop(26));
 
 
 var layout = function() {
 	
+	userHeaderTitle.text = 'Brukerinformasjon';
+	userHeader.add(userHeaderTitle);
+	userHeader.top = 1;
+	wrapper.add(userHeader);
+
 	wrapper.add(table);
 
-	wrapper.add(logoutBtn);
+	easyHeaderTitle.text = 'Enkel betaling';
+	easyHeader.add(easyHeaderTitle);
+	wrapper.add(easyHeader);
+
+	easyWrapper.add(easyLabel);
+
+	wrapper.add(easyWrapper);
+
+
+	//self.add(logoutBtn);
+	self.setRightNavButton(logoutBtn);
 
 	self.add(wrapper);
 };
@@ -76,12 +98,38 @@ function getUserInfo() {
 
 function createUserTable(user) {
 
-	var data = [
-		{title: 'Navn: '},
-		{title: 'Epost: ' + user.epost}
-	];
+	table.setData([]);
 
-	table.setData(data);
+	function createRow(lbl, txt) {
+		var row = Titanium.UI.createTableViewRow(styles.row),
+			label = Titanium.UI.createLabel(styles.label),
+			text = Titanium.UI.createLabel(styles.txt);
+
+		if(!txt) {
+			var txt = '—';
+		}
+
+		label.text = lbl;
+		text.text = txt;
+
+		row.add(label);
+		row.add(text);
+
+		debug('create row for ' + lbl + ': ' + txt);
+
+		return row;
+	};
+
+
+	// Add rows
+	var usern = user.fornavn + ' ' + user.etternavn;
+	table.appendRow(createRow('Navn', usern));
+	table.appendRow(createRow('Epost', user.epost));
+	table.appendRow(createRow('Mobil', user.mobil));
+	table.appendRow(createRow('Adresse', user.adresse1));
+	table.appendRow(createRow('', user.adresse2));
+	table.appendRow(createRow('Postnr', user.postnr));
+	table.appendRow(createRow('Poststed', user.poststed));
 
 }
 
--- Resources/ui/styles/profile/Profile.js
+++ Resources/ui/styles/profile/Profile.js
@@ -1,13 +1,79 @@
 
 exports = {
 	table: {
-		height: 300,
-		width: 320
+		height: 200,
+		top: 0,
+		backgroundColor: 'transparent'
+	},
+	tableHeader: {
+		top: 0,
+		height: 26,
+		backgroundColor: '#222'
+	},
+	tableHeaderTitle: {
+		text: 'Velg forestilling',
+		color: '#eee',
+		font: {
+			fontSize: 12,
+			fontWeight: 'bold'
+		},
+		shadowColor: '#111',
+		shadowOffset: {x: 0, y: -1},
+		width: 'auto',
+		height: 'auto',
+		left: 12
 	},
 	logoutBtn: {
 		width: 400,
 		height: 50,
 		title: 'Logg ut',
 		bottom: 0
-	}
+	},
+	row: {
+		height: 42,
+		backgroundImage: 'images/buy/table_places.png',
+		layout: 'horizontal'
+	},
+	label: {
+		font: {
+			fontSize: 14,
+			fontWeight: 'normal'
+		},
+		color: '#7b7b7e',
+		width: 125,
+		top: 12,
+		left: 15,
+		height: 'auto',
+		shadowColor: '#fff',
+		shadowOffset: {x: 0, y: 1}
+	},
+	txt: {
+		font: {
+			fontSize: 14,
+			fontWeight: 'normal'
+		},
+		color: '#464646',
+		height: 'auto',
+		width: 'auto',
+		top: 12,
+		shadowColor: '#fff',
+		shadowOffset: {x: 0, y: 1}
+	},
+	easyWrapper: {
+		height: 60,
+		backgroundImage: 'images/buy/table_places.png'
+	},
+	easyLabel: {
+		text: 'Vil du aktivere enkel betaling?',
+		font: {
+			fontSize: 14,
+			fontWeight: 'bold'
+		},
+		color: '#464646',
+		height: 'auto',
+		width: 'auto',
+		//top: 18,
+		shadowColor: '#fff',
+		shadowOffset: {x: 0, y: 1}
+	},
 };
